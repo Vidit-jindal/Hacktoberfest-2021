f3075b92d29fcf0a9ad9a2e6fede668c08b88e0a
Priyansha Nayak <87187181+PriyanshaNayak@users.noreply.github.com>
2021-10-10 21:33:06 +0530

    Print the number of words in a string

--- C++/Number_of_words.cpp
+++ C++/Number_of_words.cpp
@@ -1,22 +1,18 @@
-#include <iostream>
-#include <string.h>
-
+#include<iostream>
+#include<string.h>
 using namespace std;
-
 int main()
 {
     char str[50];
-    int count = 0, i;
-    cout << "Enter a string : ";
+    int count,i;
+    count =0;
+    cout<<"Enter a String: ";
     gets(str);
-
-    for (i = 0; str[i] != '\0'; i++)
+    for(i=0;str[i]!='\0';i++)
     {
-        if (str[i] == ' ')
-            count++;
+        if(str[i]==' ')
+        count++;
     }
-
-    cout << "Number of words in the string are: " << count + 1;
-
-    return 0;
+    cout<<"Number of words in the String are: "<<count+1;
+    return (0);
 }
